c4c8aa897169d312fb427a2040685a72a5d8efaf
unknown <pranshulsaxena09@gmail.com>
2020-11-19 11:09:19 +0530

    final

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,32 +7,32 @@
     <script src="http://cdn.ckeditor.com/4.5.11/standard/ckeditor.js"></script>
 	</head>
 	<body>
-	<nav class="navbar navbar-inverse">
-      <div class="container">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="<?php echo base_url(); ?>">Journalism Society MITS</a>
-        </div>
-        <div id="navbar">
-          <ul class="nav navbar-nav">
-            <li><a href="<?php echo base_url(); ?>">Home</a></li>
-            <li><a href="<?php echo base_url(); ?>about">About</a></li>
-             <li><a href="<?php echo base_url(); ?>posts">Blog</a></li>
-             <li><a href="<?php echo base_url(); ?>categories">Categories</a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
+	
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="<?php echo base_url(); ?>">Journalism Society MITS</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>">Home</a></li>
+      <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>about">About</a></li>
+      <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>posts">Blog</a></li>
+      <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>categories">Categories</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
           <?php if(!$this->session->userdata('logged_in')) : ?>
-            <li><a href="<?php echo base_url(); ?>users/login">Login</a></li>
-            <li><a href="<?php echo base_url(); ?>users/register">Register</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>users/login">Login</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>users/register">Register</a></li>
           <?php endif; ?>
           <?php if($this->session->userdata('logged_in')) : ?>
-            <li><a href="<?php echo base_url(); ?>posts/create">Create Post</a></li>
-            <li><a href="<?php echo base_url(); ?>categories/create">Create Category</a></li>
-            <li><a href="<?php echo base_url(); ?>users/logout">Logout</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>posts/create">Create Post</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>categories/create">Create Category</a></li>
+            <li class="nav-item"><a  class="nav-link" href="<?php echo base_url(); ?>users/logout">Logout</a></li>
           <?php endif; ?>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    </ul>
+  </div>
+</nav>
 
     <div class="container">
       <!-- Flash messages -->
